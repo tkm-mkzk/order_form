6b931ebf4d619286f95e930394d0d85017071a2d
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-16 10:49:48 +0900

    :sparkles: 小さなフォームの作成

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '3.1.2'
+ruby '3.2.1'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.0.3'
@@ -68,6 +68,7 @@ group :development, :test do
   # ソースコードの静的コード解析 bundle exec rubocopでチェックできます。
   gem 'rubocop', require: false
   gem 'rubocop-performance'
+  gem 'rubocop-rails', require: false
   # 以下、コースで追加したgem ここまで
 end
 
---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+  def new
+    @order = Order.new
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+  end
+
+  def create
+    @order = Order.new(order_params)
+    return render :new if params[:button] == 'back'
+
+    if @order.save
+      session[:order_id] = @order.id
+      return redirect_to complete_orders_url
+    end
+
+    render :confirm
+  end
+
+  def complete
+    @order = Order.find_by(id: session[:order_id])
+    return redirect_to new_order_url if @order.blank?
+
+    session[:order_id] = nil
+  end
+
+  private
+
+  def order_params
+    params
+      .require(:order)
+      .permit(:name,
+              :email,
+              :telephone,
+              :delivery_address)
+  end
+end
